480641cd12c77d5f434e3ad430d9ab9059cd1373
Leandro Lucarella <llucax@gmail.com>
2003-07-12 21:39:44 +0000

    Removed \n at the end of the file and added a VIM option to keep it as is.

--- xmi2code.tpl.php
+++ xmi2code.tpl.php
@@ -19,59 +19,9 @@
 // | along with Hooks; if not, write to the Free Software Foundation,   |
 // | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA      |
 // +--------------------------------------------------------------------+
-// | Created: Wed May 17 18:16:54 ART 2003                              |
+// | Created: @@date                                  |
 // | Authors: Leandro Lucarella <luca@lugmen.org.ar>                    |
 // +--------------------------------------------------------------------+
 //
 // $Id$
 //
-
-// +X2C includes
-require_once 'BIFE/Fallback.php';
-// ~X2C
-
-// +X2C Class 7 :Translate
-/**
- * This is a generic and simple (but very usefull) BIFE_Fallback implementation. Translate widgets using a template with it's name, prepended with 'bife_'. If not template is found, it copy the XML to the output.
- *
- * @package BIFE
- * @access public
- */
-class BIFE_Translate extends BIFE_Fallback {
-    // ~X2C
-
-    // +X2C Operation 12
-    /**
-     * Renders the widget.
-     *
-     * @param  HTML_Template_HIT &$template Template to use to render the widget.
-     *
-     * @return string
-     * @access public
-     */
-    function render(&$template) // ~X2C
-    {
-        $name = "bife_{$this->name}";
-        if ($template->exists($name, '')) {
-            $this->attrs['CONTENTS'] = $this->renderContents($template);
-            $out = $template->parse($name, $this->attrs, '', '');
-        } else {
-            $name = $this->name;
-            $out = "<$name";
-            foreach ($this->attrs as $attr => $val) {
-                $out .= sprintf(' %s="%s"', $attr, $val);
-            }
-            $contents = $this->renderContents($template);
-            if ($contents !== '') {
-                $out .= ">$contents</$name>";
-            } else {
-                $out .= "/>";
-            }
-        }
-        return $out;
-    }
-    // -X2C
-
-} // -X2C Class :Translate
-
-?>
